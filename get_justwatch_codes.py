c7de4c7b70a65635b4d9d31e1431b3c1fbfd6498
Siddharth Mathur <mathursiddharth2000@gmail.com>
2021-12-13 15:37:41 -0800

    Update get_justwatch_codes.py

--- get_justwatch_codes.py
+++ get_justwatch_codes.py
@@ -7,3 +7,8 @@ with open('movie_providers.json') as f:
 print('technical_name, short_name, clear_name, monetization_types')
 for item in data['providers']:
 	print(item.get('technical_name') + ", " + item.get('short_name') + ", " + item.get('clear_name') + ", " + str(item.get('monetization_types')))
+
+print("Free services: ", end="")
+for item in data['providers']:
+	if 'free' in item.get('monetization_types') or 'ads' in item.get('monetization_types'):
+		print("'"+item.get('short_name')+"', ", end="")
