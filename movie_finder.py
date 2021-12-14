019a340f21d7d4fe762d3f5b9d8901e1d34c00ac
Siddharth Mathur <mathursiddharth2000@gmail.com>
2021-12-13 16:28:52 -0800

    Update movie_finder.py

--- movie_finder.py
+++ movie_finder.py
@@ -4,10 +4,12 @@ import ssl
 from bs4 import BeautifulSoup
 from justwatch import JustWatch
 
+service_codes = {'nfx': 'Netflix', 'amp': 'Amazon Prime Video', 'dnp': 'Disney Plus', 'atp': 'Apple TV Plus', 'itu': 'Apple iTunes', 'fuv': 'fuboTV', 'hlu': 'Hulu', 'hbm': 'HBO Max', 'hmf': 'HBO Max Free', 'pct': 'Peacock', 'pcp': 'Peacock Premium', 'amz': 'Amazon Video', 'ply': 'Google Play Movies', 'yot': 'YouTube', 'pmp': 'Paramount Plus', 'fmn': 'Funimation Now', 'ast': 'Starz Play Amazon Channel', 'app': 'Paramount+ Amazon Channel', 'aep': 'EPIX Amazon Channel', 'adp': 'Discovery+ Amazon Channel', 'ahb': 'HBO Now Amazon Channel', 'ash': 'Showtime Amazon Channel', 'azp': 'AMC Plus', 'hbn': 'HBO Now', 'rkc': 'The Roku Channel', 'srk': 'Showtime Roku Premium Channel', 'prk': 'Paramount+ Roku Premium Channel', 'sru': 'Starz Roku Premium Channel', 'ark': 'AMC+ Roku Premium Channel', 'erk': 'Epix Roku Premium Channel', 'koc': 'Kocowa', 'ytr': 'YouTube Premium', 'yfr': 'YouTube Free', 'hop': 'Hoopla', 'tcw': 'The CW', 'cws': 'CW Seed', 'vdu': 'Vudu', 'vuf': 'VUDU Free', 'stz': 'Starz', 'crc': 'Criterion Channel', 'sho': 'Showtime', 'pbs': 'PBS', 'pfx': 'Pantaflix', 'fxn': 'FXNow', 'tbv': 'Tubi TV', 'knp': 'Kanopy', 'com': 'Comedy Central', 'msf': 'Microsoft Store', 'cru': 'Crunchyroll', 'rbx': 'Redbox', 'snx': 'Sun Nxt', 'mxg': 'Max Go', 'abc': 'ABC', 'drv': 'DIRECTV', 'crk': 'Crackle', 'amt': 'AMC Theatres', 'amc': 'AMC', 'fnd': 'Fandor', 'cts': 'Curiosity Stream', 'nbc': 'NBC', 'epx': 'Epix', 'ffm': 'Freeform', 'his': 'History', 'sfy': 'Syfy', 'aae': 'A&E', 'lft': 'Lifetime', 'shd': 'Shudder', 'scb': 'Screambox', 'act': 'Acorn TV', 'sdn': 'Sundance Now', 'pcf': 'Popcornflix', 'gdc': 'GuideDoc', 'bbo': 'BritBox', 'rlz': 'realeyz', 'mbi': 'Mubi', 'nfk': 'Netflix Kids', 'pty': 'Pantaya', 'bmg': 'Boomerang', 'umc': 'Urban Movie Channel', 'hvt': 'History Vault', 'dvc': 'Dove Channel', 'ytv': 'Yupp TV', 'ern': 'Eros Now', 'mns': 'Magnolia Selects', 'wwe': 'WWE Network', 'mot': 'MyOutdoorTV', 'anh': 'Nickhits Amazon Channel', 'ang': 'Noggin Amazon Channel', 'htv': 'Hopster TV', 'lol': 'Laugh Out Loud', 'ssc': 'Smithsonian Channel', 'pux': 'Pure Flix', 'hmm': 'Hallmark Movies', 'lmc': 'Lifetime Movie Club', 'apk': 'PBS Kids Amazon Channel', 'abo': 'Boomerang Amazon Channel', 'acn': 'Cinemax Amazon Channel', 'apa': 'Pantaya Amazon Channel', 'ahm': 'Hallmark Movies Now Amazon Channel', 'apm': 'PBS Masterpiece Amazon Channel', 'avt': 'Viewster Amazon Channel', 'ame': 'MZ Choice Amazon Channel', 'sfx': 'Spamflix', 'stv': 'Sling TV', 'ptv': 'Pluto TV', 'hdv': 'HiDive', 'vix': 'VIX ', 'nfp': 'Night Flight Plus', 'tpc': 'Topic', 'mtv': 'MTV', 'rtc': 'Retrocrush', 'cla': 'Classix', 'dkk': 'Dekkoo', 'sft': 'Shout! Factory TV', 'chf': 'Chai Flicks', 'ovi': 'OVID', 'mhz': 'Mhz Choice', 'tfd': 'The Film Detective', 'vtv': 'Vice TV ', 'asd': 'Shudder Amazon Channel', 'amu': 'Mubi Amazon Channel', 'aac': 'AcornTV Amazon Channel', 'abb': 'BritBox Amazon Channel', 'afa': 'Fandor Amazon Channel', 'asb': 'Screambox Amazon Channel', 'asn': 'Sundance Now Amazon Channel', 'ctw': 'Cartoon Network', 'ads': 'Adult Swim', 'usn': 'USA Network', 'fus': 'Fox', 'fxf': 'FlixFling', 'bpc': 'Bet+ Amazon Channel', 'vik': 'Rakuten Viki', 'amo': 'AMC on Demand', 'dkm': 'Darkmatter TV', 'tcm': 'TCM', 'brv': 'Bravo TV', 'tnt': 'TNT', 'fnw': 'Food Network', 'bca': 'BBC America', 'ind': 'IndieFlix', 'ahc': 'AHCTV', 'tlc': 'TLC', 'hgt': 'HGTV', 'diy': 'DIY Network', 'inv': 'Investigation Discovery', 'sci': 'Science Channel', 'dea': 'Destination America', 'apl': 'Animal Planet', 'dil': 'Discovery Life', 'dis': 'Discovery', 'mtr': 'Motor Trend', 'coo': 'Cooking Channel', 'tra': 'Travel Channel', 'pnw': 'Paramount Network', 'hrv': 'Here TV', 'tvl': 'TV Land', 'ltv': 'Logo TV', 'vho': 'VH1', 'fpm': 'Flix Premiere', 'adw': 'DreamWorksTV Amazon Channel', 'tus': 'TBS', 'asc': 'AsianCrush', 'flr': 'FILMRISE', 'rvy': 'Revry', 'dsv': 'DOCSVILLE', 'rst': 'Rooster Teeth', 'sod': 'Spectrum On Demand', 'oxy': 'OXYGEN', 'hyh': 'Hi-YAH', 'vrv': 'VRV', 'tru': 'tru TV', 'dnw': 'DisneyNOW', 'wet': 'WeTV', 'dpu': 'Discovery Plus', 'awp': 'ARROW', 'plx': 'Plex', 'wow': 'WOW Presents Plus', 'alm': 'Alamo on Demand', 'mgl': 'Magellan TV', 'bhd': 'BroadwayHD', 'own': 'The Oprah Winfrey Network', 'fmz': 'Filmzie', 'mvt': 'MovieSaints', 'dog': 'Dogwoof On Demand', 'trs': 'True Story', 'mst': 'Martha Stewart TV', 'daf': 'DocAlliance Films', 'bph': 'British PathÃ© TV', 'kor': 'KoreaOnDemand', 'hoc': 'Hoichoi', 'fmp': 'Film Movement Plus', 'iqi': 'iQIYI', 'mtg': 'Metrograph', 'aim': 'IMDB TV Amazon Channel', 'cur': 'Curia', 'pdm': 'Public Domain Movies', 'knw': 'Kino Now'}
 just_watch = JustWatch(country='US')
-services = ['amp', 'nfx', 'stv', 'hmf', 'pct', 'pcp', 'fmn', 'rkc', 'yfr', 'tcw', 'cws', 'vuf', 'pbs', 'tbv', 'knp', 'com', 'cru', 'abc', 'crk', 'his', 'aae', 'lft', 'pcf', 'bmg', 'dvc', 'ytv', 'mot', 'lol', 'ssc', 'ptv', 'vix', 'sft', 'tfd', 'vtv', 'ctw', 'ads', 'usn', 'vik', 'dkm', 'brv', 'fnw', 'ind', 'ahc', 'tlc', 'hgt', 'diy', 'inv', 'sci', 'dea', 'apl', 'dil', 'dis', 'coo', 'tra', 'hrv', 'ltv', 'vho', 'flr', 'rvy', 'rst', 'hyh', 'vrv', 'dpu', 'plx', 'own', 'fmz', 'bph', 'hoc', 'aim', 'pdm'] # set streaming services to search (in order)
+paid_services = ['amp', 'nfx', 'stv'] # set streaming services to search (in order)
 letterboxd_username = 'sidward35'
 watchlist_pages = 4
+
 
 # get watchlist
 watchlist_titles = []
@@ -28,21 +30,18 @@ for html_object in html_objects:
 	end_index = object_str.index('"', start_index)
 	movie_name = object_str[start_index:end_index]
 	watchlist_titles.append(movie_name)
-print(watchlist_titles)
+print(letterboxd_username+"'s watchlist: "+str(watchlist_titles))
 
 
 result = {}
-for service in services:
+for service in paid_services:
 	service_list = []
 	indexes_to_remove = []
 	for i in range(len(watchlist_titles)):
 		movie = watchlist_titles[i]
 		#year = watchlist_years[i]
 		#movies_found = just_watch.search_for_item(query=movie+' '+year, providers=[service], monetization_types=['flatrate'])
-		if service=='amp' or service=='nfx' or service=='stv': # for paid services
-			movies_found = just_watch.search_for_item(query=movie, providers=[service], monetization_types=['flatrate', 'free', 'ads'])
-		else: # for free services
-			movies_found = just_watch.search_for_item(query=movie, providers=[service], monetization_types=['free', 'ads'])
+		movies_found = just_watch.search_for_item(query=movie, providers=[service], monetization_types=['flatrate', 'free', 'ads'])
 		if len(movies_found['items'])>0 and movies_found['items'][0]['title'] == movie:
 			service_list.append(movie+' ('+str(movies_found['items'][0]['original_release_year'])+')') 
 			indexes_to_remove.append(i)
@@ -50,7 +49,24 @@ for service in services:
 		watchlist_titles.pop(i) # remove movie from overall watchlist
 		#watchlist_years.pop(i)
 
-	result[service] = service_list
+	result[service_codes[service]] = service_list
+
+free_services = ['hmf', 'pct', 'pcp', 'fmn', 'rkc', 'yfr', 'tcw', 'cws', 'vuf', 'pbs', 'tbv', 'knp', 'com', 'cru', 'abc', 'crk', 'his', 'aae', 'lft', 'pcf', 'bmg', 'dvc', 'ytv', 'mot', 'lol', 'ssc', 'ptv', 'vix', 'sft', 'tfd', 'vtv', 'ctw', 'ads', 'usn', 'vik', 'dkm', 'brv', 'fnw', 'ind', 'ahc', 'tlc', 'hgt', 'diy', 'inv', 'sci', 'dea', 'apl', 'dil', 'dis', 'coo', 'tra', 'hrv', 'ltv', 'vho', 'flr', 'rvy', 'rst', 'hyh', 'vrv', 'dpu', 'plx', 'own', 'fmz', 'bph', 'hoc', 'aim', 'pdm']
+free_movie_list = []
+indexes_to_remove = []
+for i in range(len(watchlist_titles)):
+	movie = watchlist_titles[i]
+	movies_found = just_watch.search_for_item(query=movie, providers=free_services, monetization_types=['free', 'ads'])
+	if len(movies_found['items'])>0 and movies_found['items'][0]['title'] == movie:
+		offers = movies_found['items'][0]['offers']
+		for offer in offers:
+			if offer.get('monetization_type')=='ads' or offer.get('monetization_type')=='free':
+				free_movie_list.append(service_codes[offer.get('package_short_name')]+': '+movie+' ('+str(movies_found['items'][0]['original_release_year'])+')') 
+				indexes_to_remove.append(i)
+				break
+for i in sorted(indexes_to_remove, reverse=True):
+	watchlist_titles.pop(i) # remove movie from overall watchlist
+result['Free Services'] = free_movie_list
 
 # remaining movies in watchlist ==> other
 other_list = []
