36d0a4d3eca6ad6cb4a162da6c055995951ca55f
Siddharth Mathur <mathursiddharth2000@gmail.com>
2025-03-12 01:24:59 -0700

    Update script to use new API

--- README.md
+++ README.md
@@ -6,11 +6,13 @@ Scans your [Letterboxd](https://letterboxd.com) watchlist and shows you where yo
 
 ## Usage
 1. Install `BeautifulSoup4` and `justwatch` using pip.
-2. Download [movie_finder.py](https://github.com/sidward35/letterboxd-justwatch/blob/main/movie_finder.py).
-3. Edit lines 10 and 11 to set your paid streaming services ([service codes](#service-codes) below) and Letterboxd username.
+2. Download [movie_finder.py](https://gitlab.com/sidward35/letterboxd-justwatch/-/blob/main/movie_finder.py).
+3. Edit lines 11 and 17 to set your paid streaming services ([service codes](#service-codes) below) and Letterboxd username.
 ```python
-paid_services = ['amp', 'nfx', 'stv']
+# line 11
 letterboxd_username = 'sidward35'
+# line 17
+paid_services = [9, 8, 299]  # Amazon Prime, Netflix, Sling TV
 ```
 4. Run `python movie_finder.py`
 
@@ -23,202 +25,205 @@ Like this project?
 </a>
 
 ## Service Codes
-|Streaming Service|Code|
-|:-:|:-:|
-|A&E|aae|
-|ABC|abc|
-|AHCTV|ahc|                                     |AMC|amc|
-|AMC Plus|azp|                                  |AMC Theatres|amt|
-|AMC on Demand|amo|
-|AMC+ Roku Premium Channel|ark|
-|ARROW|awp|
-|Acorn TV|act|
-|AcornTV Amazon Channel|aac|
-|Adult Swim|ads|
-|Alamo on Demand|alm|
-|Amazon Prime Video|amp|
-|Amazon Video|amz|
-|Animal Planet|apl|
-|Apple TV Plus|atp|
-|Apple iTunes|itu|
-|AsianCrush|asc|
-|BBC America|bca|
-|Bet+ Amazon Channel|bpc|
-|Boomerang|bmg|
-|Boomerang Amazon Channel|abo|
-|Bravo TV|brv|
-|BritBox|bbo|
-|BritBox Amazon Channel|abb|
-|British PathÃ© TV|bph|
-|BroadwayHD|bhd|
-|The CW|tcw|
-|CW Seed|cws|
-|Cartoon Network|ctw|
-|Chai Flicks|chf|
-|Cinemax Amazon Channel|acn|
-|Classix|cla|
-|Comedy Central|com|
-|Cooking Channel|coo|
-|Crackle|crk|
-|Criterion Channel|crc|
-|Crunchyroll|cru|
-|Curia|cur|
-|Curiosity Stream|cts|
-|DIRECTV|drv|
-|DIY Network|diy|
-|DOCSVILLE|dsv|
-|Darkmatter TV|dkm|
-|Dekkoo|dkk|
-|Destination America|dea|
-|Discovery|dis|
-|Discovery Life|dil|
-|Discovery Plus|dpu|
-|Discovery+ Amazon Channel|adp|
-|Disney Plus|dnp|
-|DisneyNOW|dnw|
-|DocAlliance Films|daf|
-|Dogwoof On Demand|dog|
-|Dove Channel|dvc|
-|DreamWorksTV Amazon Channel|adw|
-|EPIX Amazon Channel|aep|
-|Epix|epx|
-|Epix Roku Premium Channel|erk|
-|Eros Now|ern|
-|FILMRISE|flr|
-|FXNow|fxn|
-|Fandor|fnd|
-|Fandor Amazon Channel|afa|
-|The Film Detective|tfd|
-|Film Movement Plus|fmp|
-|Filmzie|fmz|
-|Flix Premiere|fpm|
-|FlixFling|fxf|
-|Food Network|fnw|
-|Fox|fus|
-|Freeform|ffm|
-|fuboTV|fuv|
-|Funimation Now|fmn|
-|Google Play Movies|ply|
-|GuideDoc|gdc|
-|HBO Max|hbm|
-|HBO Max Free|hmf|
-|HBO Now|hbn|
-|HBO Now Amazon Channel|ahb|
-|HGTV|hgt|
-|Hallmark Movies|hmm|
-|Hallmark Movies Now Amazon Channel|ahm|
-|Here TV|hrv|
-|Hi-YAH|hyh|
-|HiDive|hdv|
-|History|his|
-|History Vault|hvt|
-|Hoichoi|hoc|
-|Hoopla|hop|
-|Hopster TV|htv|
-|Hulu|hlu|
-|IMDB TV Amazon Channel|aim|
-|IndieFlix|ind|
-|Investigation Discovery|inv|
-|iQIYI|iqi|
-|Kanopy|knp|
-|Kino Now|knw|
-|Kocowa|koc|
-|KoreaOnDemand|kor|
-|Laugh Out Loud|lol|
-|Lifetime|lft|
-|Lifetime Movie Club|lmc|
-|Logo TV|ltv|
-|MTV|mtv|
-|MZ Choice Amazon Channel|ame|
-|Magellan TV|mgl|
-|Magnolia Selects|mns|
-|Martha Stewart TV|mst|
-|Max Go|mxg|
-|Metrograph|mtg|
-|Mhz Choice|mhz|
-|Microsoft Store|msf|
-|Motor Trend|mtr|
-|MovieSaints|mvt|
-|Mubi|mbi|
-|Mubi Amazon Channel|amu|
-|MyOutdoorTV|mot|
-|NBC|nbc|
-|Netflix|nfx|
-|Netflix Kids|nfk|
-|Nickhits Amazon Channel|anh|
-|Night Flight Plus|nfp|
-|Noggin Amazon Channel|ang|
-|The Oprah Winfrey Network|own|
-|OVID|ovi|
-|OXYGEN|oxy|
-|Paramount+ Showtime|pst|
-|PBS|pbs|
-|PBS Kids Amazon Channel|apk|
-|PBS Masterpiece Amazon Channel|apm|
-|Pantaflix|pfx|
-|Pantaya|pty|
-|Pantaya Amazon Channel|apa|
-|Paramount Network|pnw|
-|Paramount Plus|pmp|
-|Paramount+ Amazon Channel|app|
-|Paramount+ Roku Premium Channel|prk|
-|Peacock|pct|
-|Peacock Premium|pcp|
-|Plex|plx|
-|Pluto TV|ptv|
-|Popcornflix|pcf|
-|Public Domain Movies|pdm|
-|Pure Flix|pux|
-|Rakuten Viki|vik|
-|realeyz|rlz|
-|Redbox|rbx|
-|Retrocrush|rtc|
-|Revry|rvy|
-|The Roku Channel|rkc|
-|Rooster Teeth|rst|
-|Science Channel|sci|
-|Screambox|scb|
-|Screambox Amazon Channel|asb|
-|Shout! Factory TV|sft|
-|Showtime|sho|
-|Showtime Amazon Channel|ash|
-|Showtime Roku Premium Channel|srk|
-|Shudder|shd|
-|Shudder Amazon Channel|asd|
-|Sling TV|stv|
-|Smithsonian Channel|ssc|
-|Spamflix|sfx|
-|Spectrum On Demand|sod|
-|Starz|stz|
-|Starz Play Amazon Channel|ast|
-|Starz Roku Premium Channel|sru|
-|Sun Nxt|snx|
-|Sundance Now|sdn|
-|Sundance Now Amazon Channel|asn|
-|Syfy|sfy|
-|TBS|tus|
-|TCM|tcm|
-|TLC|tlc|
-|TNT|tnt|
-|TV Land|tvl|
-|Topic|tpc|
-|Travel Channel|tra|
-|tru TV|tru|
-|True Story|trs|
-|Tubi TV|tbv|
-|USA Network|usn|
-|Urban Movie Channel|umc|
-|VH1|vho|
-|VIX |vix|
-|VRV|vrv|
-|VUDU Free|vuf|
-|Vice TV |vtv|
-|Viewster Amazon Channel|avt|
-|Vudu|vdu|
-|WOW Presents Plus|wow|
-|WWE Network|wwe|
-|WeTV|wet|
-|YouTube|yot|
-|YouTube Free|yfr|
-|YouTube Premium|ytr|
-|Yupp TV|ytv|
+| Streaming Service | Code | Provider ID |
+|:----------------:|:----:|:-----------:|
+| A&E | aae | 156 |
+| ABC | abc | 148 |
+| AHCTV | ahc | 398 |
+| AMC | amc | 80 |
+| AMC Plus | azp | 528 |
+| AMC Theatres | amt | 162 |
+| AMC on Demand | amo | 352 |
+| AMC+ Roku Premium Channel | ark | 635 |
+| ARROW | awp | 529 |
+| Acorn TV | act | 87 |
+| AcornTV Amazon Channel | aac | 196 |
+| Adult Swim | ads | 318 |
+| Alamo on Demand | alm | 547 |
+| Amazon Prime Video | amp | 9 |
+| Amazon Video | amz | 10 |
+| Animal Planet | apl | 399 |
+| Apple TV Plus | atp | 350 |
+| Apple iTunes | itu | 2 |
+| AsianCrush | asc | 514 |
+| BBC America | bca | 397 |
+| Bet+ Amazon Channel | bpc | 343 |
+| Boomerang | bmg | 248 |
+| Boomerang Amazon Channel | abo | 288 |
+| Bravo TV | brv | 365 |
+| BritBox | bbo | 151 |
+| BritBox Amazon Channel | abb | 197 |
+| British Pathé TV | bph | 571 |
+| BroadwayHD | bhd | 554 |
+| The CW | tcw | 83 |
+| CW Seed | cws | 206 |
+| Cartoon Network | ctw | 317 |
+| Chai Flicks | chf | 438 |
+| Cinemax Amazon Channel | acn | 289 |
+| Classix | cla | 445 |
+| Comedy Central | com | 243 |
+| Cooking Channel | coo | 400 |
+| Crackle | crk | 12 |
+| Criterion Channel | crc | 258 |
+| Crunchyroll | cru | 283 |
+| Curia | cur | 617 |
+| Curiosity Stream | cts | 190 |
+| DIRECTV | drv | 358 |
+| DIY Network | diy | 405 |
+| DOCSVILLE | dsv | 475 |
+| Darkmatter TV | dkm | 355 |
+| Dekkoo | dkk | 444 |
+| Destination America | dea | 402 |
+| Discovery | dis | 403 |
+| Discovery Life | dil | 404 |
+| Discovery Plus | dpu | 520 |
+| Discovery+ Amazon Channel | adp | 584 |
+| Disney Plus | dnp | 337 |
+| DisneyNOW | dnw | 508 |
+| DocAlliance Films | daf | 569 |
+| Dogwoof On Demand | dog | 536 |
+| Dove Channel | dvc | 254 |
+| DreamWorksTV Amazon Channel | adw | 263 |
+| EPIX Amazon Channel | aep | 583 |
+| Epix | epx | 34 |
+| Epix Roku Premium Channel | erk | 636 |
+| Eros Now | ern | 218 |
+| FILMRISE | flr | 471 |
+| FXNow | fxn | 123 |
+| Fandor | fnd | 25 |
+| Fandor Amazon Channel | afa | 199 |
+| The Film Detective | tfd | 470 |
+| Film Movement Plus | fmp | 579 |
+| Filmzie | fmz | 559 |
+| Flix Premiere | fpm | 432 |
+| FlixFling | fxf | 331 |
+| Food Network | fnw | 366 |
+| Fox | fus | 328 |
+| Freeform | ffm | 211 |
+| Freevee | aim | 613 |
+| fuboTV | fuv | 257 |
+| Funimation Now | fmn | 269 |
+| Google Play Movies | ply | 3 |
+| GuideDoc | gdc | 100 |
+| HBO Max | hbm | 384 |
+| HBO Max Free | hmf | 616 |
+| HBO Now | hbn | 27 |
+| HBO Now Amazon Channel | ahb | 200 |
+| HGTV | hgt | 406 |
+| Hallmark Movies | hmm | 281 |
+| Hallmark Movies Now Amazon Channel | ahm | 290 |
+| Here TV | hrv | 417 |
+| Hi-YAH | hyh | 503 |
+| HiDive | hdv | 430 |
+| History | his | 155 |
+| History Vault | hvt | 268 |
+| Hoichoi | hoc | 315 |
+| Hoopla | hop | 212 |
+| Hopster TV | htv | 267 |
+| Hulu | hlu | 15 |
+| IMDB TV Amazon Channel | aim | 613 |
+| IndieFlix | ind | 368 |
+| Investigation Discovery | inv | 408 |
+| iQIYI | iqi | 581 |
+| Kanopy | knp | 191 |
+| Kino Now | knw | 640 |
+| Kocowa | koc | 464 |
+| KoreaOnDemand | kor | 575 |
+| Laugh Out Loud | lol | 275 |
+| Lifetime | lft | 157 |
+| Lifetime Movie Club | lmc | 284 |
+| Logo TV | ltv | 420 |
+| MTV | mtv | 453 |
+| MZ Choice Amazon Channel | ame | 291 |
+| Magellan TV | mgl | 551 |
+| Magnolia Selects | mns | 259 |
+| Martha Stewart TV | mst | 568 |
+| Max Go | mxg | 139 |
+| Metrograph | mtg | 585 |
+| Mhz Choice | mhz | 427 |
+| Microsoft Store | msf | 68 |
+| Motor Trend | mtr | 410 |
+| MovieSaints | mvt | 562 |
+| Mubi | mbi | 11 |
+| Mubi Amazon Channel | amu | 201 |
+| MyOutdoorTV | mot | 264 |
+| NBC | nbc | 79 |
+| Netflix | nfx | 8 |
+| Netflix Kids | nfk | 175 |
+| Nickhits Amazon Channel | anh | 261 |
+| Night Flight Plus | nfp | 455 |
+| Noggin Amazon Channel | ang | 262 |
+| The Oprah Winfrey Network | own | 555 |
+| OVID | ovi | 433 |
+| OXYGEN | oxy | 487 |
+| Paramount+ Showtime | pst | - |
+| PBS | pbs | 209 |
+| PBS Kids Amazon Channel | apk | 293 |
+| PBS Masterpiece Amazon Channel | apm | 294 |
+| Pantaflix | pfx | 177 |
+| Pantaya | pty | 247 |
+| Pantaya Amazon Channel | apa | 292 |
+| Paramount Network | pnw | 418 |
+| Paramount Plus | pmp | 531 |
+| Paramount+ Amazon Channel | app | 582 |
+| Paramount+ Roku Premium Channel | prk | 633 |
+| Peacock | pct | 386 |
+| Peacock Premium | pcp | 387 |
+| Plex | plx | 538 |
+| Pluto TV | ptv | 300 |
+| Popcornflix | pcf | 241 |
+| Public Domain Movies | pdm | 638 |
+| Pure Flix | pux | 278 |
+| Rakuten Viki | vik | 344 |
+| realeyz | rlz | 14 |
+| Redbox | rbx | 279 |
+| Retrocrush | rtc | 446 |
+| Revry | rvy | 473 |
+| The Roku Channel | rkc | 207 |
+| Rooster Teeth | rst | 485 |
+| Science Channel | sci | 411 |
+| Screambox | scb | 185 |
+| Screambox Amazon Channel | asb | 202 |
+| Shout! Factory TV | sft | 439 |
+| Showtime | sho | 37 |
+| Showtime Amazon Channel | ash | 203 |
+| Showtime Roku Premium Channel | srk | 632 |
+| Shudder | shd | 99 |
+| Shudder Amazon Channel | asd | 204 |
+| Sling TV | stv | 299 |
+| Smithsonian Channel | ssc | 276 |
+| Spamflix | sfx | 521 |
+| Spectrum On Demand | sod | 486 |
+| Starz | stz | 43 |
+| Starz Play Amazon Channel | ast | 194 |
+| Starz Roku Premium Channel | sru | 634 |
+| Sun Nxt | snx | 309 |
+| Sundance Now | sdn | 143 |
+| Sundance Now Amazon Channel | asn | 205 |
+| Syfy | sfy | 215 |
+| TBS | tus | 506 |
+| TCM | tcm | 361 |
+| TLC | tlc | 412 |
+| TNT | tnt | 363 |
+| TV Land | tvl | 419 |
+| Topic | tpc | 454 |
+| Travel Channel | tra | 413 |
+| tru TV | tru | 507 |
+| True Story | trs | 567 |
+| Tubi TV | tbv | 73 |
+| USA Network | usn | 322 |
+| Urban Movie Channel | umc | 251 |
+| VH1 | vho | 422 |
+| VIX | vix | 457 |
+| VRV | vrv | 504 |
+| VUDU Free | vuf | 332 |
+| Vice TV | vtv | 458 |
+| Viewster Amazon Channel | avt | 295 |
+| Vudu | vdu | 7 |
+| WOW Presents Plus | wow | 546 |
+| WWE Network | wwe | 260 |
+| WeTV | wet | 509 |
+| YouTube | yot | 192 |
+| YouTube Free | yfr | 235 |
+| YouTube Premium | ytr | 188 |
+| Yupp TV | ytv | 255 |
--- movie_finder.py
+++ movie_finder.py
@@ -1,16 +1,58 @@
 from pprint import pprint
 import urllib.request
 import ssl
+import json
 from bs4 import BeautifulSoup
-from justwatch import JustWatch
+from simplejustwatchapi.justwatch import search
 
-
-service_codes = {'nfx': 'Netflix', 'amp': 'Amazon Prime Video', 'dnp': 'Disney Plus', 'atp': 'Apple TV Plus', 'itu': 'Apple iTunes', 'fuv': 'fuboTV', 'hlu': 'Hulu', 'hbm': 'HBO Max', 'hmf': 'HBO Max Free', 'pct': 'Peacock', 'pcp': 'Peacock Premium', 'amz': 'Amazon Video', 'ply': 'Google Play Movies', 'yot': 'YouTube', 'pmp': 'Paramount Plus', 'fmn': 'Funimation Now', 'ast': 'Starz Play Amazon Channel', 'app': 'Paramount+ Amazon Channel', 'aep': 'EPIX Amazon Channel', 'adp': 'Discovery+ Amazon Channel', 'ahb': 'HBO Now Amazon Channel', 'ash': 'Showtime Amazon Channel', 'azp': 'AMC Plus', 'hbn': 'HBO Now', 'rkc': 'The Roku Channel', 'srk': 'Showtime Roku Premium Channel', 'prk': 'Paramount+ Roku Premium Channel', 'sru': 'Starz Roku Premium Channel', 'ark': 'AMC+ Roku Premium Channel', 'erk': 'Epix Roku Premium Channel', 'koc': 'Kocowa', 'ytr': 'YouTube Premium', 'yfr': 'YouTube Free', 'hop': 'Hoopla', 'tcw': 'The CW', 'cws': 'CW Seed', 'vdu': 'Vudu', 'vuf': 'VUDU Free', 'stz': 'Starz', 'crc': 'Criterion Channel', 'sho': 'Showtime', 'pbs': 'PBS', 'pfx': 'Pantaflix', 'fxn': 'FXNow', 'tbv': 'Tubi TV', 'knp': 'Kanopy', 'com': 'Comedy Central', 'msf': 'Microsoft Store', 'cru': 'Crunchyroll', 'rbx': 'Redbox', 'snx': 'Sun Nxt', 'mxg': 'Max Go', 'abc': 'ABC', 'drv': 'DIRECTV', 'crk': 'Crackle', 'amt': 'AMC Theatres', 'amc': 'AMC', 'fnd': 'Fandor', 'cts': 'Curiosity Stream', 'nbc': 'NBC', 'epx': 'Epix', 'ffm': 'Freeform', 'his': 'History', 'sfy': 'Syfy', 'aae': 'A&E', 'lft': 'Lifetime', 'shd': 'Shudder', 'scb': 'Screambox', 'act': 'Acorn TV', 'sdn': 'Sundance Now', 'pcf': 'Popcornflix', 'gdc': 'GuideDoc', 'bbo': 'BritBox', 'rlz': 'realeyz', 'mbi': 'Mubi', 'nfk': 'Netflix Kids', 'pty': 'Pantaya', 'bmg': 'Boomerang', 'umc': 'Urban Movie Channel', 'hvt': 'History Vault', 'dvc': 'Dove Channel', 'ytv': 'Yupp TV', 'ern': 'Eros Now', 'mns': 'Magnolia Selects', 'wwe': 'WWE Network', 'mot': 'MyOutdoorTV', 'anh': 'Nickhits Amazon Channel', 'ang': 'Noggin Amazon Channel', 'htv': 'Hopster TV', 'lol': 'Laugh Out Loud', 'ssc': 'Smithsonian Channel', 'pux': 'Pure Flix', 'hmm': 'Hallmark Movies', 'lmc': 'Lifetime Movie Club', 'apk': 'PBS Kids Amazon Channel', 'abo': 'Boomerang Amazon Channel', 'acn': 'Cinemax Amazon Channel', 'apa': 'Pantaya Amazon Channel', 'ahm': 'Hallmark Movies Now Amazon Channel', 'apm': 'PBS Masterpiece Amazon Channel', 'avt': 'Viewster Amazon Channel', 'ame': 'MZ Choice Amazon Channel', 'sfx': 'Spamflix', 'stv': 'Sling TV', 'ptv': 'Pluto TV', 'hdv': 'HiDive', 'vix': 'VIX ', 'nfp': 'Night Flight Plus', 'tpc': 'Topic', 'mtv': 'MTV', 'rtc': 'Retrocrush', 'cla': 'Classix', 'dkk': 'Dekkoo', 'sft': 'Shout! Factory TV', 'chf': 'Chai Flicks', 'ovi': 'OVID', 'mhz': 'Mhz Choice', 'tfd': 'The Film Detective', 'vtv': 'Vice TV ', 'asd': 'Shudder Amazon Channel', 'amu': 'Mubi Amazon Channel', 'aac': 'AcornTV Amazon Channel', 'abb': 'BritBox Amazon Channel', 'afa': 'Fandor Amazon Channel', 'asb': 'Screambox Amazon Channel', 'asn': 'Sundance Now Amazon Channel', 'ctw': 'Cartoon Network', 'ads': 'Adult Swim', 'usn': 'USA Network', 'fus': 'Fox', 'fxf': 'FlixFling', 'bpc': 'Bet+ Amazon Channel', 'vik': 'Rakuten Viki', 'amo': 'AMC on Demand', 'dkm': 'Darkmatter TV', 'tcm': 'TCM', 'brv': 'Bravo TV', 'tnt': 'TNT', 'fnw': 'Food Network', 'bca': 'BBC America', 'ind': 'IndieFlix', 'ahc': 'AHCTV', 'tlc': 'TLC', 'hgt': 'HGTV', 'diy': 'DIY Network', 'inv': 'Investigation Discovery', 'sci': 'Science Channel', 'dea': 'Destination America', 'apl': 'Animal Planet', 'dil': 'Discovery Life', 'dis': 'Discovery', 'mtr': 'Motor Trend', 'coo': 'Cooking Channel', 'tra': 'Travel Channel', 'pnw': 'Paramount Network', 'hrv': 'Here TV', 'tvl': 'TV Land', 'ltv': 'Logo TV', 'vho': 'VH1', 'fpm': 'Flix Premiere', 'adw': 'DreamWorksTV Amazon Channel', 'tus': 'TBS', 'asc': 'AsianCrush', 'flr': 'FILMRISE', 'rvy': 'Revry', 'dsv': 'DOCSVILLE', 'rst': 'Rooster Teeth', 'sod': 'Spectrum On Demand', 'oxy': 'OXYGEN', 'hyh': 'Hi-YAH', 'vrv': 'VRV', 'tru': 'tru TV', 'dnw': 'DisneyNOW', 'wet': 'WeTV', 'dpu': 'Discovery Plus', 'awp': 'ARROW', 'plx': 'Plex', 'wow': 'WOW Presents Plus', 'alm': 'Alamo on Demand', 'mgl': 'Magellan TV', 'bhd': 'BroadwayHD', 'own': 'The Oprah Winfrey Network', 'fmz': 'Filmzie', 'mvt': 'MovieSaints', 'dog': 'Dogwoof On Demand', 'trs': 'True Story', 'mst': 'Martha Stewart TV', 'daf': 'DocAlliance Films', 'bph': 'British PathÃ© TV', 'kor': 'KoreaOnDemand', 'hoc': 'Hoichoi', 'fmp': 'Film Movement Plus', 'iqi': 'iQIYI', 'mtg': 'Metrograph', 'aim': 'IMDB TV Amazon Channel', 'cur': 'Curia', 'pdm': 'Public Domain Movies', 'knw': 'Kino Now', 'pst': 'Paramount+ Showtime'}
-just_watch = JustWatch(country='US')
-paid_services = ['amp', 'nfx', 'stv'] # set streaming services to search (in order)
+# Configuration
+country = 'US'
+language = 'en'
 letterboxd_username = 'sidward35'
 
+# URL to the provider map JSON file (GitLab raw URL)
+PROVIDER_MAP_URL = "https://raw.githubusercontent.com/sidward35/letterboxd-justwatch/refs/heads/main/provider_map.json"
+
+# Preferred streaming services by ID
+paid_services = [9, 8, 299]  # Amazon Prime, Netflix, Sling TV
+
+def get_provider_map():
+	"""Fetch the provider map from the remote URL, with fallback to local defaults"""
+	try:
+		# Create context for SSL
+		ssl_context = ssl._create_unverified_context()
+		req = urllib.request.Request(PROVIDER_MAP_URL, headers={'User-Agent': "Python Movie Finder"})
+		
+		# Fetch the provider map JSON
+		with urllib.request.urlopen(req, context=ssl_context) as response:
+			data = json.loads(response.read().decode('utf-8'))
+			
+			# Convert keys from strings to integers
+			provider_map = {int(k): v for k, v in data['provider_map'].items()}
+			return provider_map
+			
+	except Exception as e:
+		print(f"Warning: Could not fetch provider map from {PROVIDER_MAP_URL}. Using minimal defaults.")
+		print(f"Error: {e}")
+		
+		# Minimal fallback provider map with key streaming services
+		return {
+			8: "Netflix",
+			9: "Amazon Prime Video",
+			299: "Sling TV",
+			207: "The Roku Channel",
+			300: "Pluto TV",
+			123: "FXNow",
+			73: "Tubi TV",
+			613: "Freevee",
+			212: "Hoopla",
+			191: "Kanopy",
+			538: "Plex",
+			486: "Spectrum On Demand"
+		}
+
 def get_page_count(username):
+	"""Get the number of pages in the user's watchlist"""
 	html_str = ''
 	url = "https://letterboxd.com/"+username+"/watchlist"
 	req = urllib.request.Request(url, headers={'User-Agent': "Magic Browser"}) 
@@ -19,61 +61,104 @@ def get_page_count(username):
 	fp.close()
 
 	soup = BeautifulSoup(html_str, 'html.parser')
-	html_objects = soup.findAll('span', {'class': 'js-watchlist-count'})
+	html_objects = soup.find_all('span', {'class': 'js-watchlist-count'})
 	base_str = str(html_objects[0])
 	page_count = int(base_str.replace('<span class="js-watchlist-count">', '').replace('\xa0films</span>', ''))
 	return int(page_count/(7*4)+1)
 
-# get watchlist
-watchlist_titles = []
-watchlist_pages = get_page_count(letterboxd_username)
-#watchlist_years = []
+def get_watchlist(username):
+	"""Get the Letterboxd watchlist for the given username"""
+	watchlist_titles = []
+	watchlist_pages = get_page_count(username)
 
-ssl._create_default_https_context = ssl._create_unverified_context
-html_str = ''
-for i in range(watchlist_pages):
-	url = "https://letterboxd.com/"+letterboxd_username+"/watchlist/by/popular/page/"+str(i+1)
-	req = urllib.request.Request(url, headers={'User-Agent': "Magic Browser"}) 
-	fp = urllib.request.urlopen(req)
-	html_str+=fp.read().decode("utf8")
-	fp.close()
+	ssl._create_default_https_context = ssl._create_unverified_context
+	html_str = ''
+	for i in range(watchlist_pages):
+		url = "https://letterboxd.com/"+username+"/watchlist/by/popular/page/"+str(i+1)
+		req = urllib.request.Request(url, headers={'User-Agent': "Magic Browser"}) 
+		fp = urllib.request.urlopen(req)
+		html_str+=fp.read().decode("utf8")
+		fp.close()
 
-soup = BeautifulSoup(html_str, 'html.parser')
-html_objects = soup.findAll('li', {'class': 'poster-container'})
-for html_object in html_objects:
-	object_str = str(html_object).replace('&amp;','&')
-	start_index = object_str.index('<img alt="')+10
-	end_index = object_str.index('"', start_index)
-	movie_name = object_str[start_index:end_index]
-	watchlist_titles.append(movie_name)
-print(letterboxd_username+"'s watchlist: "+str(watchlist_titles))
+	soup = BeautifulSoup(html_str, 'html.parser')
+	html_objects = soup.find_all('li', {'class': 'poster-container'})
+	for html_object in html_objects:
+		object_str = str(html_object).replace('&amp;','&')
+		start_index = object_str.index('<img alt="')+10
+		end_index = object_str.index('"', start_index)
+		movie_name = object_str[start_index:end_index]
+		watchlist_titles.append(movie_name)
+	
+	return watchlist_titles
 
-
-result = {}
-for movie in watchlist_titles:
-	movies_found = just_watch.search_for_item(query=movie, monetization_types=['flatrate', 'free', 'ads'])
-	if len(movies_found['items'])>0 and movies_found['items'][0]['title'] == movie:
-		movie_found = False
-		for offer in movies_found['items'][0]['offers']:
-			if offer['monetization_type']=='flatrate' and offer['package_short_name'] in paid_services:
-				movie_found = True
-				try:
-					result[service_codes[offer['package_short_name']]].append(movie+' ('+str(movies_found['items'][0]['original_release_year'])+')')
-				except:
-					result[service_codes[offer['package_short_name']]] = [movie+' ('+str(movies_found['items'][0]['original_release_year'])+')']
-				break
-			elif offer['monetization_type'] in ['free','ads']:
-				movie_found = True
-				try:
-					result['Free Services'].append(service_codes[offer.get('package_short_name')]+': '+movie+' ('+str(movies_found['items'][0]['original_release_year'])+')')
-				except:
-					result['Free Services'] = [service_codes[offer.get('package_short_name')]+': '+movie+' ('+str(movies_found['items'][0]['original_release_year'])+')']
-				break
-		if not movie_found:
+def process_watchlist(watchlist_titles, provider_map, paid_services):
+	"""Process the watchlist and find streaming services for each movie"""
+	result = {}
+	for movie in watchlist_titles:
+		# Search for the movie using the API
+		search_results = search(movie, country, language, 1, False)  # Get first result, include all offers
+		
+		if search_results and search_results[0].title == movie:
+			movie_found = False
+			movie_item = search_results[0]
+			# Try different attribute for release year
 			try:
-				result['Other'].append(movie+' ('+str(movies_found['items'][0]['original_release_year'])+')')
-			except:
-				result['Other'] = [movie+' ('+str(movies_found['items'][0]['original_release_year'])+')']
+				release_year = movie_item.production_year
+			except AttributeError:
+				try:
+					release_year = movie_item.release_year
+				except AttributeError:
+					release_year = "N/A"
+			
+			# Check all offers for the movie
+			for offer in movie_item.offers:
+				# Get provider ID from the package object
+				if not hasattr(offer, 'package') or not hasattr(offer.package, 'package_id'):
+					continue
+					
+				provider_id = offer.package.package_id
+				
+				# Check if the offer is for streaming (flatrate)
+				if offer.monetization_type == 'FLATRATE' and provider_id in paid_services:
+					movie_found = True
+					service_name = provider_map.get(provider_id, f"Provider {provider_id}")
+					
+					if service_name not in result:
+						result[service_name] = []
+					result[service_name].append(f"{movie} ({release_year})")
+					break
+					
+				# Check if the offer is free or ad-supported
+				elif offer.monetization_type in ['FREE', 'ADS']:
+					movie_found = True
+					service_name = provider_map.get(provider_id, f"Provider {provider_id}")
+					
+					if 'Free Services' not in result:
+						result['Free Services'] = []
+					result['Free Services'].append(f"{service_name}: {movie} ({release_year})")
+					break
+			
+			# If no matching service found, add to "Other" category
+			if not movie_found and movie_item.offers:
+				if 'Other' not in result:
+					result['Other'] = []
+				result['Other'].append(f"{movie} ({release_year})")
+	
+	return result
 
+def main():
+	# Load provider map
+	provider_map = get_provider_map()
+	
+	# Get watchlist
+	watchlist_titles = get_watchlist(letterboxd_username)
+	print(letterboxd_username+"'s watchlist: "+str(watchlist_titles))
 
-pprint(result)
+	# Process watchlist and find streaming services
+	result = process_watchlist(watchlist_titles, provider_map, paid_services)
+	
+	# Print results
+	pprint(result)
+
+if __name__ == "__main__":
+	main()
